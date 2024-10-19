c3b8da485eb7d39f92e46009b249ced2e5f79bc2
joel-jcs <joeljcs@protonmail.com>
2024-10-19 18:06:45 -0400

    added gif to each card via fetch api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./components/Card";
+import { API } from "./assets/data.js";
 
 function App() {
+  const getCardId = () => crypto.randomUUID();
+
+  const [fetchedData, setFetchedData] = useState(null);
+  const [cards, setCards] = useState([]);
+
+  const fetchPictures = async () => {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      setFetchedData(data);
+
+      const cardsData = data.data.map((item) => ({
+        name: item.title.replace(/GIF by .+$/, "").trim(),
+        image: item.images.original.url,
+        id: getCardId(),
+      }));
+      setCards(cardsData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPictures();
+  }, []);
+
   return (
     <>
       <div className="pageContents">
@@ -11,19 +39,16 @@ function App() {
           <span>Personal Best:</span>
         </header>
         <div className="gameboard">
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
-          <div className="card">{"Card Name"}</div>
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              id={card.id}
+              image={card.image}
+              name={card.name}
+            />
+          ))}
         </div>
+        <button onClick={() => console.log(fetchedData)}>Get Pictures</button>
       </div>
     </>
   );
